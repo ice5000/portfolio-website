e36e3e4f1dce6806a72af64adf813908c5146728
ice5000 <95354975+ice5000@users.noreply.github.com>
2023-09-21 13:28:28 +0100

    changes All

---
+++ components/HeroSection.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { TypeAnimation } from 'react-type-animation';
+
+const HeroSection = () => {
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 pt-20 sm:grid-cols-12">
+        <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal  font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600  ">Hello, I'm{" "} </span>
+            <br></br>
+            <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Isaac Ray',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'Web Developer',
+        1000,
+        'Mobile Developer',
+        1000,
+        'UI/UX Designer',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      
+      repeat={Infinity}
+    />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6  lg:text-xl ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum.
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 bg-white hover:bg-slate-200 text-white">
+              Hire Me
+            </button>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
+              Download CV
+            </button>
+          </div>
+        </div>
+        <div className="col-span-4 place-self-center mt-4 lg:mt-0">
+          
+            <img
+              className=" w-[250px] h-[250px]  rounded-full lg:w-[300px] lg:h-[300px] "
+              src="/NFT.jpg"
+              alt="/"
+            />
+          
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
